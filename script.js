8f0794e186360b47930dbcb4f58e7718627d2c76
Bravo4149 <bravo.makafu@gmail.com>
2025-07-19 20:26:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -153,7 +153,7 @@ function updateCartUI() {
   if (totalQty > 0) {
     courierFee = 180;
     if (totalQty > 2) {
-      courierFee += (totalQty - 2) * 12;
+      courierFee += (totalQty - 2) * 15;
     }
   }
 
